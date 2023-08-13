79c006df5b533173b1186b645fa4a29af13d5f0e
krtglsabiha <136503685+krtglsabiha@users.noreply.github.com>
2023-08-14 00:34:51 +0200

    Update again again

--- src/main/java/day02_datatypes/C01Variables.java
+++ src/main/java/day02_datatypes/C01Variables.java
@@ -7,6 +7,7 @@ public class C01Variables {
         byte stdAge = 25;
         System.out.println(stdAge);
         System.out.println(stdAge);
+        System.out.println(stdAge);
 
 
         short numOfStdInHighSchool = 3245;
